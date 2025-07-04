b4692af1155b5014b805b4d13fdca4eabd4353d3
Devarshini <118905256+Sylvievy@users.noreply.github.com>
2025-07-04 07:28:45 +0530

    Modify to method

--- practiceProblems/src/main/java/org/example/Pattern1.java
+++ practiceProblems/src/main/java/org/example/Pattern1.java
@@ -10,14 +10,21 @@ print the following pattern for a given integer N:
  */
 
 public class Pattern1 {
-    public static void main(String[] args) {
-
-        for (int i = 1; i < 6; i++) {
-            for (int j = 1; j < 6; j++) {
+    public static void nPattern(int n){
+        System.out.println("N="+n);
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < n+1; j++) {
                 System.out.print('*');
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        nPattern(2);
+        nPattern(4);
+        nPattern(3);
+        nPattern(7);
+
     }
 }
 
