2a31770ccd4cdf64e9a6f09a17c3d18915b32120
Devarshini <118905256+Sylvievy@users.noreply.github.com>
2025-07-04 07:42:16 +0530

    Correct naming & op conventions

--- practiceProblems/src/main/java/org/example/Pattern1.java
+++ practiceProblems/src/main/java/org/example/Pattern1.java
@@ -10,20 +10,20 @@ print the following pattern for a given integer N:
  */
 
 public class Pattern1 {
-    public static void nPattern(int n){
+    public static void BlockPattern(int n){
         System.out.println("N="+n);
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < n+1; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 System.out.print('*');
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        nPattern(2);
-        nPattern(4);
-        nPattern(3);
-        nPattern(7);
+        BlockPattern(2);
+        BlockPattern(4);
+        BlockPattern(3);
+        BlockPattern(7);
 
     }
 }
--- practiceProblems/src/main/java/org/example/Pattern2.java
+++ practiceProblems/src/main/java/org/example/Pattern2.java
@@ -10,11 +10,11 @@ Given an integer N, print the following pattern :
  */
 
 public class Pattern2 {
-    public static void hypotenusePattern(int n){
+    public static void rightTrianglePattern(int n){
         System.out.println("N="+n);
 
-        for (int i=1;i<n+1;i++){
-            for (int j=1;j<i+1;j++){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
@@ -22,10 +22,10 @@ public class Pattern2 {
     }
 
     public static void main(String[] args){
-        hypotenusePattern(4);
-        hypotenusePattern(6);
-        hypotenusePattern(2);
-        hypotenusePattern(8);
+        rightTrianglePattern(4);
+        rightTrianglePattern(6);
+        rightTrianglePattern(2);
+        rightTrianglePattern(8);
 
 
 
