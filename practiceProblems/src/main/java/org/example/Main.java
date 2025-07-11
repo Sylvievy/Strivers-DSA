d10ebcb4f222e32b08943eb96d470aa44bc3d68f
Devarshini <118905256+Sylvievy@users.noreply.github.com>
2025-07-11 08:28:34 +0530

    Star Pyramid

--- practiceProblems/src/main/java/org/example/Main.java
+++ practiceProblems/src/main/java/org/example/Main.java
@@ -2,12 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args){
-//        NumberPatterns.numberStaircase(5);
-//        StarPatterns.rightTrianglePattern(5);
-//        StarPatterns.BlockPattern(5);
-//        NumberPatterns.numStepStaircase(5);
-//        StarPatterns.invertedRightTriangle(5);
+        NumberPatterns.numberStaircase(5);
+        StarPatterns.rightTrianglePattern(5);
+        StarPatterns.BlockPattern(5);
+        NumberPatterns.numStepStaircase(5);
+        StarPatterns.invertedRightTriangle(5);
         NumberPatterns.invertedRightNumberPyramid(5);
+        StarPatterns.starPyramid(5);
 
     }
 
--- practiceProblems/src/main/java/org/example/StarPatterns.java
+++ practiceProblems/src/main/java/org/example/StarPatterns.java
@@ -11,8 +11,8 @@ package org.example;
 
 public class StarPatterns {
 
-    public static void BlockPattern(int n){
-        System.out.println("Pattern : Block | N = "+n);
+    public static void BlockPattern(int n) {
+        System.out.println("Pattern : Block | N = " + n);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 System.out.print('*');
@@ -29,16 +29,17 @@ public class StarPatterns {
     ****
     *****
     */
-    public static void rightTrianglePattern(int n){
-        System.out.println("Pattern : Right Triangle | N = "+n);
+    public static void rightTrianglePattern(int n) {
+        System.out.println("Pattern : Right Triangle | N = " + n);
 
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=i;j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+
     /*
      * * * * *
      * * * *
@@ -46,17 +47,33 @@ public class StarPatterns {
      * *
      *
      */
-    public static void invertedRightTriangle(int n){
-        System.out.println("Pattern : Inverted Right Triangle | N = "+n);
-        for (int i=1;i<=n;i++){
-            for (int j=n;j>=i;j--){
+    public static void invertedRightTriangle(int n) {
+        System.out.println("Pattern : Inverted Right Triangle | N = " + n);
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-
+    /*
+     *
+     ***
+     *****
+     */
+    public static void starPyramid(int n) {
+        System.out.println("Pattern : Star Pyramid  | N = " + n);
+        for (int i=1; i <= n; i++){
+            for(int j=1; j <= n-i ;j++){
+                System.out.print(' ');
+            }
+            for(int j=1; j <= 2*i-1; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
 }
 
 
