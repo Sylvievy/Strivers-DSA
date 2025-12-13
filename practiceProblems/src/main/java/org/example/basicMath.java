9076227ed88be1b894af76d28f83ba4a9d0fa07b
Devarshini <118905256+Sylvievy@users.noreply.github.com>
2025-12-13 08:16:18 +0530

    Refactor code -Palindrome

--- practiceProblems/src/main/java/org/example/basicMath.java
+++ practiceProblems/src/main/java/org/example/basicMath.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -16,46 +17,56 @@ public class basicMath {
     }
 
     public static void GCD(int a, int b){
-        int min=0;
-        int GCD=0;
-
-        if(a>b){
-            min=b;
-        }else{
-            min=a;
+        int min,max=0;
+        min=a<b? a:b;
+        max=a>b? a:b;
+        while(min!=0){
+            int temp=min;
+            min=max%min;
+            max=temp;
         }
-        for(int i=1;i<=min;i++){
-            if(a%i==0 && b%i==0){
-                if(i>GCD){
-                    GCD=i;
-                }
-            }
-        }
-        System.out.println("The GCD of the given two integers "+a+" "+b+" is "+GCD);
+        System.out.println("The GCD of the given two integers "+a+","+b+" is "+max);
     }
 
-    public static boolean armstrong(int n){
-        String num=String.valueOf(n);
-        int x=0;
-        int numLen=num.length();
-        for(int i=1;i<numLen;i++){
-            x=(int)num.charAt(i)*numLen;
+    public static boolean armstrong(int n) {
+        int originalN = n;
+        int sum = 0;
+
+        int k = String.valueOf(n).length();
+
+        while (n > 0) {
+            int x = n % 10;
+            sum += (int) Math.pow(x, k);
+            n /= 10;
         }
-        if(x==n){
-            return true;
-        }
-        return false;
+        return sum == originalN;
     }
 
     public static void divisors(int n){
         ArrayList<Integer> divisors=new ArrayList<>();
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 divisors.add(i);
+                if(i!=n/i) divisors.add(n/i);
             }
         }
-        System.out.println("The divisors of "+n+" are "+divisors);
+        Collections.sort(divisors);
+        System.out.println("The divisors of "+n+" are "+ divisors);
     }
+
+    public static void palindrome(int n){
+        int given=n;
+        String checkNum="";
+        while(n>0){
+            int x=n%10;
+            checkNum+=String.valueOf(x);
+            n/=10;
+        }
+        String result=(checkNum.equals(String.valueOf(given)))?"a Palindrome":"not a Palindrome";
+        System.out.println("The given number "+given+" is "+result);
+    }
+
+
 
     public static void main(String[] args){
         System.out.print("Enter a number:");
@@ -64,8 +75,9 @@ public class basicMath {
 //        int y=sc.nextInt();
 //        countDigits(x);
 //        GCD(x,y);
-        System.out.println(armstrong(x));
-        divisors(x);
+//        System.out.println(armstrong(x));
+//        divisors(x);
+        palindrome(x);
 
     }
 }
